c4f2746c145d30b6a1a0f99dae4143c80d503c78
xElementzx <xelementzx1@gmail.com>
2021-01-21 17:37:02 +1100

    Add version to startup notifications

--- src/main/java/me/elementz/enchantingbooks/EnchantingBooks.java
+++ src/main/java/me/elementz/enchantingbooks/EnchantingBooks.java
@@ -25,9 +25,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
 import org.apache.commons.lang3.StringUtils;
@@ -41,6 +39,7 @@ import java.util.Map;
 public class EnchantingBooks {
 
     public static final String ID = "enchantingbooks";
+    public static final String VERSION = "@version@";
     private static final Logger LOGGER = LogManager.getLogger();
 
     public EnchantingBooks() {
@@ -53,27 +52,13 @@ public class EnchantingBooks {
         BlockRegistration.register();
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
-        LOGGER.info("[Enchanting Books] - PreInit");
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-//        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("[Enchanting Books] - Server Starting");
+        LOGGER.info("[Enchanting Books] - Hit Pre Init - " + VERSION);
     }
 
     @SubscribeEvent
